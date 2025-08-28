01b8784e005a177dbf7d16f0ce9e22a555ff3636
geekp2p <geekp2p@gmail.com>
2025-08-28 09:20:26 +0700

    Delete run-node.bat

--- run-node.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd /d C:\Users\Administrator\p2p-mesh
-REM Default chat room must match the Docker/config.yaml setup
-set APP_ROOM=my-room
-set ENABLE_RELAY_CLIENT=1
-set ENABLE_HOLEPUNCH=0
-set ENABLE_UPNP=0
-.\bin\node.exe
